516ac7de5829a21ecf78834ce8a29654c094cf2c
Hilder Cesar <hilder_cesar@hotmail.com>
2022-05-13 10:54:25 -0300

    new layout done

--- src/app/modules/private/private-routing.module.ts
+++ src/app/modules/private/private-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DateTime } from 'luxon';
 
 // Container
 import { PrivateComponent } from './private.component';
